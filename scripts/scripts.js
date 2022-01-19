2ce33f2adaeca575dbc282c01eea7a5b68aee07a
JoseSissa <53451389+JoseSissa@users.noreply.github.com>
2022-01-19 17:58:55 -0500

    home screen

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const hamburguer = document.getElementById('hamburguer');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
+hamburguer.addEventListener('click', ()=>{
+    console.log('click');
+});

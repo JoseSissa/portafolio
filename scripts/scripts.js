cb66d21a54cbcc4fc8892ecd9aa9ab1c0048097d
JoseSissa <53451389+JoseSissa@users.noreply.github.com>
2022-01-21 20:54:32 -0500

    menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,13 @@
 const hamburguer = document.getElementById('hamburguer');
 const menu = document.getElementById('menu');
+const menuList = document.getElementById('menu-list');
 
 hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle('show');
     hamburguer.classList.toggle('toogle_open');
 });
+
+menuList.addEventListener('click', (e)=>{
+    menu.classList.toggle('show');
+    hamburguer.classList.toggle('toogle_open');
+})
